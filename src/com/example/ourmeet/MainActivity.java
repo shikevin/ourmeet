b64603f23630744eda13647ccb9dfa948a62caff
Kevin Shi <kevin@dare2tee.com>
2013-12-24 19:09:50 -0500

    mainActivity now tracks location of user using coarse and fine tracking

--- src/com/example/ourmeet/MainActivity.java
+++ src/com/example/ourmeet/MainActivity.java
@@ -1,23 +1,64 @@
 package com.example.ourmeet;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.mapquest.android.maps.GeoPoint;
+import com.mapquest.android.maps.MapActivity;
+import com.mapquest.android.maps.MapView;
+import com.mapquest.android.maps.MyLocationOverlay;
 
+public class MainActivity extends MapActivity {
+	protected MapView map;
+	private MyLocationOverlay myLocationOverlay;
+	
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        setupMapView();
+        setupMyLocation();
     }
     
+    //set your map and enable default zoom controls
+    private void setupMapView(){
+    	this.map = (MapView) findViewById(R.id.map);
+    	map.setBuiltInZoomControls(true);
+    }
+    
+    //set up a MyLocationOverlay and execute the runnable once we have a location fix
+    private void setupMyLocation() {
+    	this.myLocationOverlay = new MyLocationOverlay(this,map);
+    	myLocationOverlay.enableMyLocation();
+    	myLocationOverlay.runOnFirstFix(new Runnable () {
+    		@Override
+    		public void run() {
+    			GeoPoint currentLocation = myLocationOverlay.getMyLocation();
+    			map.getController().animateTo(currentLocation);
+    			map.getController().setZoom(14);
+    			map.getOverlays().add(myLocationOverlay);
+    			myLocationOverlay.setFollowing(true);
+    		}
+    	});
+    }
+    
+    //enable features of the overlay
+    @Override
+    protected void onResume() {
+    	myLocationOverlay.enableMyLocation();
+    	myLocationOverlay.enableCompass();
+    	super.onResume();
+    }
+    
+    //disable features of the overlay when in the background
+    @Override
+    protected void onPause(){
+    	super.onPause();
+    	myLocationOverlay.disableCompass();
+    	myLocationOverlay.disableMyLocation();
+    }
+    
+    @Override
+    public boolean isRouteDisplayed() {
+    	return false;
+    }
 }
